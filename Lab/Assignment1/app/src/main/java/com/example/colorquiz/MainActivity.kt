22a2041721dbcc35a82c1134e9e36ea26889d461
Martin Kuchar <kucharmartin0@gmail.com>
2020-09-03 09:36:27 -0700

    Finish the assignment

--- Lab/Assignment1/app/src/main/java/com/example/colorquiz/MainActivity.kt
+++ Lab/Assignment1/app/src/main/java/com/example/colorquiz/MainActivity.kt
@@ -19,29 +19,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     var colors: HashMap<String, Int> = hashMapOf(
-        "black" to R.color.black,
-        "red" to com.example.colorquiz.R.color.red,
-        "green" to com.example.colorquiz.R.color.green,
-        "yellow" to com.example.colorquiz.R.color.yellow,
-        "blue" to com.example.colorquiz.R.color.blue,
-        "gray" to com.example.colorquiz.R.color.gray,
-        "cyan" to com.example.colorquiz.R.color.cyan,
-        "pink" to com.example.colorquiz.R.color.pink,
-        "purple" to com.example.colorquiz.R.color.purple,
-        "orange" to com.example.colorquiz.R.color.orange
+        "black" to R.color.black, "red" to R.color.red, "green" to R.color.green,
+        "yellow" to R.color.yellow, "blue" to R.color.blue, "gray" to R.color.gray,
+        "cyan" to R.color.cyan, "pink" to R.color.pink, "purple" to R.color.purple,
+        "orange" to R.color.orange
     )
 
     var arr: ArrayList<String> = arrayListOf(
-        "black",
-        "red",
-        "green",
-        "yellow",
-        "blue",
-        "gray",
-        "cyan",
-        "pink",
-        "purple",
-        "orange"
+        "black", "red", "green", "yellow", "blue",
+        "gray", "cyan", "pink", "purple", "orange"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,15 +46,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun questionAnswered(view: View) {
-        question.score = (question.score.toInt() + 1).toString()
-        val toast = Toast.makeText(applicationContext, "Correct", Toast.LENGTH_LONG)
-        toast.show()
-//            question.score = (question.score.toInt() - 1).toString()
-//            val toast = Toast.makeText(applicationContext, "Incorrect", Toast.LENGTH_LONG)
-//            toast.show()
-        setQuestion()
-        binding.invalidateAll()
-
+        if (arr.isNotEmpty()) {
+            if (view.tag == question.answerString) {
+                question.score = (question.score.toInt() + 1).toString()
+                val toast = Toast.makeText(applicationContext, "Correct", Toast.LENGTH_LONG)
+                toast.show()
+            } else {
+                question.score = (question.score.toInt() - 1).toString()
+                val toast = Toast.makeText(applicationContext, "Incorrect", Toast.LENGTH_LONG)
+                toast.show()
+            }
+            setQuestion()
+            binding.invalidateAll()
+        }
     }
 
     fun setQuestion() {
@@ -78,12 +68,14 @@ class MainActivity : AppCompatActivity() {
             val rsString = arr.removeAt(rnd.nextInt(arr.size - 1))
             question.lsColor = ContextCompat.getColor(this, colors.remove(lsString)!!)
             question.rsColor = ContextCompat.getColor(this, colors.remove(rsString)!!)
-            val answerInt = rnd.nextInt(1)
-            question.answer = if (answerInt == 1) question.rsColor else question.lsColor
+            val answerInt = rnd.nextInt(2)
             question.answerString = if (answerInt == 1) rsString else lsString
-            print(arr.size)
+            question.lsString = lsString
+            question.rsString = rsString
         } else {
             question.score = "Final score is: " + question.score
+            binding.lsButton.isEnabled = false
+            binding.rsButton.isEnabled = false
         }
     }
 }
--- Lab/Assignment1/app/src/main/java/com/example/colorquiz/Question.kt
+++ Lab/Assignment1/app/src/main/java/com/example/colorquiz/Question.kt
@@ -10,24 +10,7 @@ class Question {
     var score: String = "0"
     var lsColor: Int = 0
     var rsColor: Int = 0
-    var answer: Int = 0
+    var lsString: String = ""
+    var rsString: String = ""
     var answerString: String = ""
-
-    var arr: ArrayList<String> = arrayListOf("black", "red", "green", "yellow", "blue", "gray", "cyan", "pink", "purple", "orange")
-
-    fun setQuestion() {
-        if (arr.size > 2) {
-            val rnd = Random
-            val lsString = arr.removeAt(rnd.nextInt(arr.size - 1))
-            val rsString = arr.removeAt(rnd.nextInt(arr.size - 1))
-//            lsColor = colors.remove(lsString)!!
-//            rsColor = colors.remove(rsString)!!
-            val answerInt = rnd.nextInt(1)
-            answer = if (answerInt == 1) rsColor else lsColor
-            answerString =  if (answerInt == 1) rsString else lsString
-            print(arr.size)
-        } else {
-            score = "Final score is: " + this.score
-        }
-    }
 }
