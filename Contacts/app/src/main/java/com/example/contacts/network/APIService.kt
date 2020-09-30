80c3ef3de3d7e3ad711fe049b48fa17c59207a0b
Martin Kuchar <kucharmartin0@gmail.com>
2020-09-29 17:59:09 -0700

    Create retrofit object and API interface

--- Contacts/app/src/main/java/com/example/contacts/network/APIService.kt
+++ Contacts/app/src/main/java/com/example/contacts/network/APIService.kt
@@ -1,2 +1,28 @@
 package com.example.contacts.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL = "https://randomuser.me/api/"
+
+// 1. create a retrofit object
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+//  2. API interface
+interface ContactsApiService {
+    @GET("contacts")
+    fun getProperties():
+            Call<String>
+}
+
+// 3. Singleton object
+object ContactsAPI {
+    val retrofitService : ContactsApiService by lazy {
+        retrofit.create(ContactsApiService::class.java) }
+}
